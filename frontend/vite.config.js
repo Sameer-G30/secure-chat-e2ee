da731ff294b11e4d9686d813aaccfdde0b32ac62
Vrati <aryavrati2306@gmail.com>
2026-08-22 12:27:17 +0530

    test1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['libsodium-wrappers']
+  },
+  resolve: {
+    alias: {
+      // Use the correct export path for libsodium
+      'libsodium-wrappers': 'libsodium-wrappers/dist/modules-sumo/libsodium-wrappers.js'
+    }
+  }
 })
